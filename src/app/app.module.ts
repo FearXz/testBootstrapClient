8f018d95eb652e5d5990afc36b4f2f95ee87ca8c
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-05-28 15:13:31 +0200

    chore: Refactor app component structure
    ```

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './comp/navbar/navbar.component';
+import { FormComponent } from './comp/form/form.component';
+import { FormButtonComponent } from './comp/form-button/form-button.component';
+import { ResultAreaComponent } from './comp/result-area/result-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FormComponent,
+    FormButtonComponent,
+    ResultAreaComponent
   ],
   imports: [
     BrowserModule,
